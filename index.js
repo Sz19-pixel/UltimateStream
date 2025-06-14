480876310466a352c182eaf8d6c6aece161c75fa
Sz19-pixel <trytry11101@gmail.com>
2025-06-14 17:45:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { ScraperManager } = require('./src/scrapers/ScraperManager');
-const { TorrentManager } = require('./src/torrents/TorrentManager');
+const  ScraperManager  = require('./src/scrapers/ScraperManager');
+const  TorrentManager  = require('./src/torrents/TorrentManager');
 
 // Enable CORS for all routes
 app.use(cors());
